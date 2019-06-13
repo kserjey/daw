b32204cfc2b4e35c8f11b15cbd8ff63b34a58fa4
kserjey <k.serjey@gmail.com>
2019-06-13 18:42:00 +0300

    feat: header and font-family

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import Tone from 'tone';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components/macro';
+import { Header } from './components/Header';
 import { BeatSequencer } from './components/BeatSequencer';
 import { PianoRoll } from './components/PianoRoll';
 
-function useBPM(initialValue = 128) {
-  const [bpm, setBPM] = useState(initialValue);
-
-  useEffect(() => {
-    Tone.Transport.bpm.value = bpm;
-  }, [bpm]);
-
-  return [bpm, setBPM];
-}
-
 const DRUM_KIT = [
-  { name: 'Kick', source: `${process.env.PUBLIC_URL}/sounds/808-Kicks01.wav` },
+  {
+    name: 'Kick',
+    source: `${process.env.PUBLIC_URL}/sounds/808-Kicks01.wav`
+  },
   {
     name: 'HiHat',
     source: `${process.env.PUBLIC_URL}/sounds/808-HiHats03.wav`
   },
-  { name: 'Snare', source: `${process.env.PUBLIC_URL}/sounds/808-Snare02.wav` }
+  {
+    name: 'Snare',
+    source: `${process.env.PUBLIC_URL}/sounds/808-Snare02.wav`
+  }
 ];
 
-function App() {
-  const [bpm, setBPM] = useBPM();
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    height: 100%;
+    margin: 0;
+    font-family: 'Heebo', sans-serif;
+  }
+`;
 
+function App() {
   return (
     <React.Fragment>
-      <input
-        type="number"
-        value={bpm}
-        onChange={({ target }) => setBPM(parseInt(target.value))}
-      />
+      <GlobalStyle />
+      <Header style={{ marginBottom: 16 }} />
       <BeatSequencer
         style={{ marginBottom: 16 }}
         drumKit={DRUM_KIT}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import Tone from 'tone';
+import styled from 'styled-components/macro';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+  padding: 0 16px;
+  background-color: #4d4d4d;
+  color: #ffffff;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-right: 8px;
+`;
+
+const Input = styled.input`
+  box-sizing: border-box;
+  width: 72px;
+  height: 32px;
+  padding: 0 8px;
+  border: 1px solid #ffffff;
+  border-radius: 2px;
+  background-color: #666666;
+  color: #ffffff;
+  font-size: 15px;
+  font-family: inherit;
+`;
+
+function useBPM(initialValue = 128) {
+  const [bpm, setBPM] = useState(initialValue);
+
+  useEffect(() => {
+    Tone.Transport.bpm.value = bpm;
+  }, [bpm]);
+
+  return [bpm, setBPM];
+}
+
+function Header({ style, className }) {
+  const [bpm, setBPM] = useBPM();
+  const handleChange = useCallback(event => setBPM(event.target.value), []);
+
+  return (
+    <Container {...{ style, className }}>
+      <Label>Tempo (BPM):</Label>
+      <Input type="number" value={bpm} onChange={handleChange} />
+    </Container>
+  );
+}
+
+export { Header };
