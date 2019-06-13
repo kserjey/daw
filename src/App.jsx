f7801c008f8c0415f5685ccda3ec65665b0a8459
kserjey <k.serjey@gmail.com>
2019-06-13 19:14:15 +0300

    fix: margins and paddings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components/macro';
+import styled, { createGlobalStyle } from 'styled-components/macro';
 import { Header } from './components/Header';
 import { BeatSequencer } from './components/BeatSequencer';
 import { PianoRoll } from './components/PianoRoll';
@@ -19,6 +19,10 @@ const DRUM_KIT = [
   }
 ];
 
+const Container = styled.div`
+  padding: 32px;
+`;
+
 const GlobalStyle = createGlobalStyle`
   html, body {
     height: 100%;
@@ -32,13 +36,15 @@ function App() {
   return (
     <React.Fragment>
       <GlobalStyle />
-      <Header style={{ marginBottom: 16 }} />
-      <BeatSequencer
-        style={{ marginLeft: 32, marginBottom: 16 }}
-        drumKit={DRUM_KIT}
-        stepsLength={16}
-      />
-      <PianoRoll />
+      <Header />
+      <Container>
+        <BeatSequencer
+          style={{ marginBottom: 32 }}
+          drumKit={DRUM_KIT}
+          stepsLength={16}
+        />
+        <PianoRoll />
+      </Container>
     </React.Fragment>
   );
 }
