21bac69288cdd42c002f5c1628e2b138e1f7fb25
kserjey <k.serjey@gmail.com>
2019-06-14 03:23:03 +0300

    feat: piano roll view

--- src/components/PianoRoll/PianoRoll.jsx
+++ src/components/PianoRoll/PianoRoll.jsx
@@ -1,62 +1,94 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/macro';
 
-const LANE_HEIGHT = 32;
-const STEP_WIDTH = 32;
-
-const Pattern = styled.svg`
-  border: 1px solid black;
+const Divider = styled.line`
+  stroke: #666666;
+  stroke-width: 2px;
 `;
 
-const Step = styled.rect(
-  ({ noteIndex, stepIndex }) => `
-    width: ${STEP_WIDTH}px;
-    height: ${LANE_HEIGHT}px;
-    x: ${stepIndex * STEP_WIDTH}px;
-    y: ${noteIndex * LANE_HEIGHT}px;
-    fill: grey;
-  `
-);
+const NoteText = styled.text`
+  fill: #ffffff;
+  dominant-baseline: middle;
+  text-anchor: middle;
+`;
 
-const VerticalLine = styled.rect(
-  ({ number }) => `
-    width: 2px;
-    height: 100%;
-    x: ${STEP_WIDTH * number}px;
-    y: 0;
-  `
-);
+// function NoteLabel({ height }) {
 
-const HorizontalLine = styled.rect(
-  ({ number }) => `
-    width: 100%;
-    height: 2px;
-    x: 0;
-    y: ${LANE_HEIGHT * number}px;
-  `
-);
+// }
 
-const NOTES = ['C', 'D', 'F', 'G', 'A', 'B', 'C'];
-const STEP_LENGTH = 32;
-
-const NOTES_STATE = [{ note: 'C4', duration: '4n', time: '0' }];
-
-function usePianoRoll() {
-  const [notesState, setNotesState] = useState();
-}
-
-function PianoRoll() {
-  const height = NOTES.length * LANE_HEIGHT;
-  const width = STEP_LENGTH * STEP_WIDTH;
+function NotesLabels({ x, y, width, notes, noteHeight }) {
+  const height = notes.length * noteHeight;
 
   return (
-    <Pattern width={width} height={height}>
-      {NOTES.map((note, noteIndex) =>
-        Array.from({ length: STEP_LENGTH }).map((_, stepIndex) => (
-          <Step noteIndex={noteIndex} stepIndex={stepIndex} />
-        ))
-      )}
-    </Pattern>
+    <svg
+      style={{ marginRight: 1, borderRadius: '4px 0 0 4px' }}
+      {...{ width, height, x, y }}
+    >
+      {notes.map((note, noteIndex) => (
+        <svg width="100%" height={noteHeight} x={0} y={noteIndex * noteHeight}>
+          <rect width="100%" height="100%" fill="#858585" />
+          <NoteText x="50%" y="50%">
+            {note}
+          </NoteText>
+        </svg>
+      ))}
+      <g>
+        {Array.from({ length: notes.length + 1 }).map((_, rowIndex) => (
+          <Divider
+            x1={0}
+            y1={rowIndex * noteHeight}
+            x2="100%"
+            y2={rowIndex * noteHeight}
+          />
+        ))}
+      </g>
+    </svg>
+  );
+}
+
+function Grid({ x, y, rows, columns, rowHeight, columnWidth }) {
+  const width = columns * columnWidth;
+  const height = rows * rowHeight;
+
+  return (
+    <svg style={{ borderRadius: '0 4px 4px 0' }} {...{ width, height, x, y }}>
+      <rect width="100%" rowHeight="100%" fill="#858585" />
+      {Array.from({ length: rows + 1 }).map((_, rowIndex) => (
+        <Divider
+          x1={0}
+          y1={rowIndex * rowHeight}
+          x2="100%"
+          y2={rowIndex * rowHeight}
+        />
+      ))}
+      {Array.from({ length: columns + 1 }).map((_, columnIndex) => (
+        <Divider
+          x1={columnIndex * columnWidth}
+          y1={0}
+          x2={columnIndex * columnWidth}
+          y2="100%"
+        />
+      ))}
+    </svg>
+  );
+}
+
+const NOTES = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'];
+
+const STEP_WIDTH = 72;
+const ROW_HEIGHT = 32;
+
+function PianoRoll({ stepsLength = 16 }) {
+  return (
+    <div>
+      <NotesLabels width={64} notes={NOTES} noteHeight={ROW_HEIGHT} />
+      <Grid
+        rows={NOTES.length}
+        rowHeight={ROW_HEIGHT}
+        columns={stepsLength}
+        columnWidth={STEP_WIDTH}
+      />
+    </div>
   );
 }
 
